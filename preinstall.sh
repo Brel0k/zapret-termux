6aea808325a4b7bfbf0687aeab146af5877b97e0
Brel0k <breel0k.mail@gmail.com>
2025-08-26 17:46:09 +0300

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,10 +1,10 @@
 pkg install tsu python git coreutils
 pkg upgrade
 
-git clone https://github.com/Brel0k/zapret-termux.git
+git clone https://github.com/Brel0k/zapret-termux.git zapret-termux
 
 cd zapret-termux
 
-chmode +x main.sh
+chmod +x main.sh
 
 ./main.sh
